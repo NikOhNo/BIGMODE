c1f236b2da2d95dcecdc1f3b4e378cb98a7129be
Scott Mesdjian <scottmesdjian@Scotts-MacBook-Air.local>
2023-11-25 00:03:24 -0800

    Menu Work

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadSceneName(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void LoadNextScene() 
+    {
+        int currIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextIndex = currIndex + 1;
+
+        if (nextIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextIndex);
+        }
+        else
+        {
+            Debug.Log("Reached end of scenes, returning to scene 0");
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
